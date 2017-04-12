853ae98500eeef255eae3d0a8b4a9fd5137516ef
xaoc2nd <mathieuguery76@gmail.com>
2017-04-12 16:52:26 +0200

    push du total

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -2,6 +2,11 @@
 package example;
 
 import org.apache.commons.lang.WordUtils;
+import java.io.FileReader;
+import com.opencsv.CSVReader;
+import java.util.List;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Hello {
     public static void main(String[] args) {
@@ -9,6 +14,33 @@ public class Hello {
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
+        int nb=0;
+        int monmax = 0 ;
+        try {
+		CSVReader reader = new CSVReader(new FileReader("data.csv")) ;
+		List<String[]> myEntries = reader.readAll() ;
+		for (String[] ligne : myEntries) {
+
+			for (String colonne : ligne) 
+		    		{
+		    			System.out.println("nombre lu--" + colonne+"--");
+
+		    			if (colonne.length() != 0){
+		    				nb++;
+			    			if (Integer.parseInt(colonne) > monmax) 
+			    			{
+			    				monmax = Integer.parseInt(colonne);
+			    			}
+			    		}
+		    		}
+		}
+		}
+		catch(IOException e)
+		{
+			System.out.println("Pas de fichier");
+		}
+		System.out.println("nombre total lu: " + nb);
+		System.out.println("nombre max du tableau: " + monmax);
     }
 }
         
